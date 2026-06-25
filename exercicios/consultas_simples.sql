ba5eb189011bec780c953f4f12f18cf87b328134
Igor Barbosa Ferreira <igorbarbosaferreira@gmail.com>
2026-06-25 17:39:31 -0300

    exercicios select where order by like in null between

--- exercicios/consultas_simples.sql
+++ exercicios/consultas_simples.sql
@@ -1,15 +1,15 @@
 --1. Somente o nome de todos os vendedores em ordem alfabética.
-select * from vendedor order by nome;
+select nome from vendedor order by nome;
 
 --2. Os produtos que o preço seja maior que R$200,00, em ordem crescente pelo preço.
-select * from produto where valor > 200 order by valor;
+select nome, valor from produto where valor > 200 order by valor;
 
 --3. O nome do produto, o preço e o preço reajustado em 10%, ordenado pelo nome do produto.
-select nome, valor, round(valor * 1.10, 2) as valor_reajustado from produto order by nome; 
+select nome, valor, round(valor + (valor * 10) / 100, 2) as valor_reajustado from produto order by nome; 
 
 --4. Os municípios do Rio Grande do Sul.
-select * from municipio;
-select * from municipio where uf = 'RS';
+select * from uf;
+select * from municipio where iduf = 5;
 
 --5. Os pedidos feitos entre 10/04/2008 e 25/04/2008 ordenado pelo valor.
 select * from pedido;
@@ -19,7 +19,7 @@ select * from pedido where data_pedido between '2008-04-10' and '2008-04-25' ord
 select * from pedido where valor between 1000 and 1500;
 
 --7. Os pedidos que o valor não esteja entre R$100,00 e R$500,00.
-select * from pedido where valor between 100 and 500;
+select * from pedido where valor not between 100 and 500;
 
 --8. Os pedidos do vendedor André ordenado pelo valor em ordem decrescente.
 select * from vendedor;
@@ -42,25 +42,35 @@ select * from pedido where idvendedor = 5 or idvendedor = 7;
 
 --13. Os clientes que moram em União da Vitória ou Porto União.
 select * from municipio;
-select * from cliente where idmunicipio = 9 or idmunicipio = 1;
+select * from cliente where idmunicipio = 1 or idmunicipio = 9;
 
 --14. Os clientes que não moram em União da Vitória e nem em Porto União.
 select * from cliente where idmunicipio not in (9, 1);
 
 --15. Os clientes que não informaram o logradouro.
+select nome, logradouro from cliente where logradouro is null;
 
 --16. Os clientes que moram em avenidas.
+select nome, logradouro from cliente where logradouro like 'Av%';
 
 --17. Os vendedores que o nome começa com a letra S.
+select * from vendedor where nome like 'S%';
 
 --18. Os vendedores que o nome termina com a letra A.
+select * from vendedor where nome like '%a';
 
 --19. Os vendedores que o nome não começa com a letra A.
+select * from vendedor where nome not like 'A%';
 
 --20. Os municípios que começam com a letra P e são de Santa Catarina.
+select * from uf;
+select * from municipio where nome like 'P%' and iduf = 1;
 
 --21. As transportadoras que informaram o endereço.
+select * from transportadora;
+select * from transportadora where logradouro is not null;
 
 --22. Os itens do pedido 01.
-
+select * from pedido_produto; where idpedido = 1;
 --23. Os itens do pedido 06 ou do pedido 10.
+select * from pedido_produto where idpedido = 6 or idpedido = 10;
