a2116d6e558d38a55857c3d331a314f74d29430d
igorbarbosaf <igorbarbosaferreira@gmail.com>
2026-08-04 17:03:59 -0300

    Exercício subconsultas part1

--- exercicios/subconsultas.sql
+++ exercicios/subconsultas.sql
@@ -1,11 +1,53 @@
 --1. O nome dos clientes que moram na mesma cidade do Manoel. Não deve ser mostrado o Manoel.
+select
+	nome,
+	idmunicipio
+from
+	cliente
+where
+	idmunicipio = (select idmunicipio from cliente where nome = 'Manoel')
+and
+	idcliente <> 1;
+		
 
 --2. A data e o valor dos pedidos que o valor do pedido seja menor que a média de todos os pedidos.
+select
+	data_pedido,
+	valor
+from
+	pedido
+where
+	valor < (select avg(valor) from pedido);
 
 --3. A data,o valor, o cliente e o vendedor dos pedidos que possuem 2 ou mais produtos.
+select
+	pdd.data_pedido,
+	pdd.valor,
+	cln.nome as cliente,
+	vnd.nome as vendedor,
+from
+	pedido pdd
+left outer join
+	cliente cln on pdd.idcliente = cln.idcliente
+left outer join
+	vendedor vnd on pdd.idvendedor = vnd.idvendedor
+where
+	(select sum(quantidade) from pedido_produto pdp where pdp.idpedido = pdd.idpedido) >= 2;
+
+select * from pedido_produto;
+
 
 --4. O nome dos clientes que moram na mesma cidade da transportadora BSTransportes.
+select
+	nome,
+	idmunicipio
+from
+	cliente
+where
+	idmunicipio = (select idmunicipio from transportadora where idtransportadora = 1);
 
+select * from transportadora;
+	
 --5. O nome do cliente e o município dos clientes que estão localizados no mesmo município de qualquer uma das transportadoras.
 
 --6. Atualizar o valor do pedido em 5% para os pedidos que o somatório do valor total dos produtos daquele pedido seja maior que a média do valor total
